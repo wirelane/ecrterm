92c9b7eb5812c0a2d106f79787f75efa8cd07c03
László Károlyi <laszlo@karolyi.hu>
2018-04-22 20:14:14 +0200

    Adding setup tools for real

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+from ecrterm import __version__
+
+setup(
+    name='py3-ecrterm',
+    version=__version__,
+    packages=find_packages(exclude=['ecrterm.tests']),
+    license='LGPL-3',
+    install_requires=['pyserial'],
+    classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or '
+        'later (LGPLv3+)',
+    ],
+)
