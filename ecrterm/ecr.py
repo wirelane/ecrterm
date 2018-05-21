ce753d9937ace77a4436f8879837ac9df4d83cf6
László Károlyi <laszlo@karolyi.hu>
2018-05-21 17:00:42 +0200

    Return completion instead of payment True on payment success

--- ecrterm/ecr.py
+++ ecrterm/ecr.py
@@ -282,7 +282,7 @@ class ECR(object):
             # now check if the packet actually got what it wanted.
             if self.transmitter.last.completion:
                 if isinstance(self.transmitter.last.completion, Completion):
-                    return True
+                    return self.transmitter.last.completion
             else:
                 return False
         else:
